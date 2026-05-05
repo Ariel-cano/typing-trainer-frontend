0c8ec6df6a1ca6b5d0ddc7bc7dc72854b134a235
Ariel-cano <vaskaivanov856@gmail.com>
2026-05-05 20:51:27 +0400

    feat: enhance about developers section with team information and styling

--- src/app/pages/welcome/about-developers/about-developers.component.ts
+++ src/app/pages/welcome/about-developers/about-developers.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import {NzCardComponent} from 'ng-zorro-antd/card';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-about-developers',
-  imports: [],
+  imports: [
+    NzCardComponent,
+    NgOptimizedImage
+  ],
   templateUrl: './about-developers.component.html',
   styleUrl: './about-developers.component.scss',
   standalone: true
