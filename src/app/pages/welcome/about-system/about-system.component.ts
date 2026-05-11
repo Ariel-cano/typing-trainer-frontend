844c9b8efaeb7b3424ff859c58248ea2bd59ac05
Ariel-cano <vaskaivanov856@gmail.com>
2026-05-11 14:50:07 +0400

    feat: enhance about system page with detailed sections and styling

--- src/app/pages/welcome/about-system/about-system.component.ts
+++ src/app/pages/welcome/about-system/about-system.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
+import { NzAnchorModule } from 'ng-zorro-antd/anchor';
+import { NzFloatButtonModule } from 'ng-zorro-antd/float-button';
+import { NzCardModule } from 'ng-zorro-antd/card';
 
 @Component({
   selector: 'app-about-system',
-  imports: [],
+  imports: [CommonModule, NzCardModule, NzAnchorModule, NzFloatButtonModule, NgOptimizedImage],
   templateUrl: './about-system.component.html',
   styleUrl: './about-system.component.scss',
   standalone: true
